564e372601456980927cf44ddbd4dfb4aa76fee6
Jonathan Harris <jonathansimon.harris@gmail.com>
2025-11-27 12:39:24 -0300

    thanks camila

---
+++ app/models/bookmark.rb
@@ -0,0 +1,10 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+
+  validates :movie_id, presence: true, uniqueness: { scope: :list_id, message: "is already in the list" }
+  validates :comment, presence: true, length: { minimum: 6 }
+  validates :list, presence: true
+
+
+end
